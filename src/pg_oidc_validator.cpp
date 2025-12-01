1dd5309812785dcdb46eea21e2e3ef9501548959
Praveen Gajulapalli <13733716+pkgajulapalli@users.noreply.github.com>
2025-12-01 14:05:50 +0530

    fixed serialization

--- src/pg_oidc_validator.cpp
+++ src/pg_oidc_validator.cpp
@@ -79,7 +79,7 @@ bool validate_token(const ValidatorModuleState* state, const char* token, const 
   }
 
   const auto jwks_uri = issuer_object.at("jwks_uri").to_str();
-  elog(LOG, "Found issuer_object: %s", serialize_map_to_string(issuer_object));
+  elog(LOG, "Found issuer_object: %s", serialize_map_to_string(issuer_object).c_str());
 
   if (jwks_uri.empty()) {
     elog(WARNING, "Could not parse JWKS URI from issuer configuration");
